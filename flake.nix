59ef14e8b2db54c2aacce54d0c2c04e2f4ce1dc5
Richard Taesler <officiall.xdragonyt@gmail.com>
2026-03-12 21:35:56 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Eden emulator flake build";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    eden-src = {
+      url = "git+https://git.eden-emu.dev/eden-emu/eden?ref=master";
+      flake = false;
+    };
+    # CPM deps that are often missing in nixpkgs as ready CMake packages
+    mcl-src = {
+      url = "git+https://github.com/azahar-emu/mcl?rev=7b08d83418f628b800dfac1c9a16c3f59036fbad";
+      flake = false;
+    };
+    sirit-src = {
+      url = "git+https://github.com/eden-emulator/sirit?rev=4aa0fe9f2ca0b31d8345f89c8ca2757156ff2393";
+      flake = false;
+    };
+    frozen-src = {
+      url = "git+https://github.com/serge-sans-paille/frozen?rev=61dce5ae18ca59931e27675c468e64118aba8744";
+      flake = false;
+    };
+    tzdb-src = {
+      url = "https://git.crueter.xyz/misc/tzdb_to_nx/releases/download/121125/121125.tar.gz";
+      flake = false;
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    eden-src,
+    mcl-src,
+    sirit-src,
+    frozen-src,
+    tzdb-src,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      cpmCustomSources = [
+        {
+          name = "mcl";
+          src = mcl-src;
+          dir = "mcl";
+          cmakeVar = "mcl_CUSTOM_DIR";
+        }
+        {
+          name = "sirit";
+          src = sirit-src;
+          dir = "sirit";
+          cmakeVar = "sirit_CUSTOM_DIR";
+        }
+        {
+          name = "frozen";
+          src = frozen-src;
+          dir = "frozen";
+          cmakeVar = "frozen_CUSTOM_DIR";
+        }
+      ];
+    in {
+      packages.default = import ./nix/eden-package.nix {
+        inherit pkgs cpmCustomSources;
+        edenSrc = eden-src;
+        tzdbPath = tzdb-src;
+      };
+      packages.eden = self.packages.${system}.default;
+      apps.default = flake-utils.lib.mkApp {
+        drv = self.packages.${system}.default;
+        exePath = "/bin/eden";
+      };
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [self.packages.${system}.default];
+      };
+    });
+}
---
+++ nix/eden-package.nix
@@ -0,0 +1,101 @@
+{ pkgs
+, edenSrc
+, cpmCustomSources ? []
+, tzdbPath ? null
+}:
+let
+  lib = pkgs.lib;
+
+  cpmCopyCommands = lib.concatMapStringsSep "\n" (pkg: ''
+    cp -r --no-preserve=mode,ownership ${pkg.src} "$TMPDIR/${pkg.dir}"
+    chmod -R u+w "$TMPDIR/${pkg.dir}"
+  '') cpmCustomSources;
+
+  cpmCustomFlags = lib.concatMapStringsSep "\n" (pkg:
+    ''    "-D${pkg.cmakeVar}=$TMPDIR/${pkg.dir}"''
+  ) cpmCustomSources;
+
+  tzdbFlag = lib.optionalString (tzdbPath != null) ''
+    "-DYUZU_TZDB_PATH=${tzdbPath}"
+  '';
+in
+pkgs.clangStdenv.mkDerivation {
+  pname = "eden";
+  version = "git";
+  src = edenSrc;
+
+  nativeBuildInputs = with pkgs; [
+    cmake
+    ninja
+    pkg-config
+    qt6.wrapQtAppsHook
+    python3
+    git
+  ];
+
+  buildInputs = with pkgs; [
+    openssl
+    boost
+    fmt
+    nlohmann_json
+    lz4
+    zlib
+    zstd
+    enet
+    libopus
+    mbedtls
+    vulkan-headers
+    vulkan-utility-libraries
+    vulkan-loader
+    spirv-tools
+    spirv-headers
+    vulkan-memory-allocator
+    glslang
+    ffmpeg-headless
+    libusb1
+    cubeb
+    httplib
+    cpp-jwt
+    simpleini
+    unordered_dense
+    frozen
+    xbyak
+    SDL2
+    qt6.qtbase
+    qt6.qtmultimedia
+    qt6.qtwayland
+    qt6.qttools
+    qt6.qt5compat
+    qt6Packages.quazip
+    gamemode
+    discord-rpc
+  ];
+
+  preConfigure = ''
+    # CPM may patch these sources -> needs writable copy
+    ${cpmCopyCommands}
+
+    cmakeFlagsArray+=(
+${cpmCustomFlags}
+${tzdbFlag}
+    )
+  '';
+
+  cmakeFlags = [
+    "-GNinja"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DYUZU_TESTS=OFF"
+    "-DYUZU_USE_CPM=OFF"
+    "-DCPMUTIL_FORCE_SYSTEM=ON"
+    "-Dxbyak_FORCE_SYSTEM=ON"
+    "-Dunordered_dense_FORCE_SYSTEM=ON"
+    "-Dsimpleini_FORCE_SYSTEM=ON"
+    "-Dmbedtls_FORCE_SYSTEM=ON"
+    "-Denet_FORCE_SYSTEM=ON"
+    "-DENABLE_QT=ON"
+    "-DENABLE_SDL2=ON"
+  ];
+
+  # wrapQtAppsHook wraps /bin/eden for Qt runtime vars
+  dontWrapQtApps = false;
+}
